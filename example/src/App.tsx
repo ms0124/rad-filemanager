896a592848ac3cc88f3c05240557d2597c160fc5
ms0124 <moslemkar124@gmail.com>
2023-02-12 17:38:20 +0330

    use less component i removed that in feutuer

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { FileManagerReact } from 'rad-filemanager';
-import 'rad-filemanager/dist/index.css';
+// import { FileManagerReact } from 'rad-filemanager';
+// import 'rad-filemanager/dist/index.css';
 
 const App = () => {
-  return (
-    <FileManagerReact
-      config={{ height: '800px' }}
-      header={{
-        clientId: '17959574q2f0347718971594ccd86f3f4',
-        accessToken: 'ba1bfb8e0b504a05899c319348ec70ac.XzIwMjMy'
-      }}
-    />
+  return (null
+    // <FileManagerReact
+    //   config={{ height: '800px' }}
+    //   header={{
+    //     clientId: '17959574q2f0347718971594ccd86f3f4',
+    //     accessToken: 'ba1bfb8e0b504a05899c319348ec70ac.XzIwMjMy'
+    //   }}
+    // />
   );
 };
 
