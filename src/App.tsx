53160a03ccc7de820b77cf5f0cace5a12b37465a
ms0124 <moslemkar124@gmail.com>
2023-02-12 17:38:40 +0330

    add client and header to config

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ import React, { FunctionComponent } from 'react';
 import { FileManagerReact } from './index';
 
 const App: FunctionComponent = () => {
-  return <FileManagerReact />;
+  return (
+    <FileManagerReact
+      header={{
+        accessToken: '030c42facbe043019c00f251cd57245c.XzIwMjMy',
+        clientId: '17959574q2f0347718971594ccd86f3f4'
+      }}
+      config={{ height: '500px' }}
+    />
+  );
   // return <div>1234567890</div>;
 };
 
