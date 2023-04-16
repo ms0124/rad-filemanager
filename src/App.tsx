8adcaeaee8dc609ad86eadec26659e02e55fd692
ms0124 <moslemkar124@gmail.com>
2023-04-16 10:03:19 +0430

    add permissions

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,25 @@ const App: FunctionComponent = () => {
   return (
     <FileManagerReact
       header={{
-        accessToken: '030c42facbe043019c00f251cd57245c.XzIwMjMy',
-        clientId: '17959574q2f0347718971594ccd86f3f4'
+        clientId: '17959574q2f0347718971594ccd86f3f4',
+        accessToken: '0a6865394c2445e18af3734980b3322d.XzIwMjM0'
       }}
-      config={{ height: '500px' }}
+      permissions={[
+        'drives_upload',
+        'drives_upload_image',
+        'drives_search',
+        'drives_storage',
+        'drives_folder_create',
+        'drives_cut',
+        'drives_copy',
+        'drives_delete',
+        'drives_rename',
+        'drives_folder_children',
+        'drives_archive_list'
+      ]}
+      config={{ height: '400px' }}
     />
   );
-  // return <div>1234567890</div>;
 };
 
 export default App;
