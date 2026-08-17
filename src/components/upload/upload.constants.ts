06432fc952d593b0f103747c15958a79aa23414a
ms0124 <moslemkar124@gmail.com>
2026-08-17 13:31:27 +0330

    remove 192

--- src/components/upload/upload.constants.ts
+++ src/components/upload/upload.constants.ts
@@ -1,2 +1,2 @@
-export const audioQualities = ['64', '128', '192', '256', '320'];
+export const audioQualities = ['64', '128', '256', '320'];
 export const videoQualities = ['240', '360', '480', '720', '1080'];
